c62c05a4bed411f02f21b2101a1de27a37e59590
LuizFelipe <LuizFelipe.Carmona@gmail.com>
2026-04-15 15:11:34 -0300

    Produto.java atualizado

--- pubbunkerback/src/main/java/com/pubbunker/model/produto.java
+++ pubbunkerback/src/main/java/com/pubbunker/model/produto.java
@@ -1,12 +1,11 @@
 package com.pubbunker.model;
+
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "produto")
 public class produto {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,5 +14,65 @@ public class produto {
     private String descricao;
     private Double preco;
     private String categoria;
-    private Boolean ativo = true;
+    private Boolean ativo;
+
+    public produto() {
+    }
+
+    public produto(Long id, String nome, String descricao, Double preco, String categoria, Boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+        this.ativo = ativo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
 }
