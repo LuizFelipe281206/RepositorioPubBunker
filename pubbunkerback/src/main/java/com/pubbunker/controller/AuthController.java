62ec3b5e672ab48294e91ffaf7fe7463cdfc97a4
LuizFelipe <LuizFelipe.Carmona@gmail.com>
2026-08-06 15:07:33 -0300

    atualização authcontroller

--- pubbunkerback/src/main/java/com/pubbunker/controller/AuthController.java
+++ pubbunkerback/src/main/java/com/pubbunker/controller/AuthController.java
@@ -1,44 +1,52 @@
 package com.pubbunker.controller;
+
 import com.pubbunker.dto.LoginRequestDTO;
 import com.pubbunker.dto.LoginResponseDTO;
+import com.pubbunker.exception.RecursoNaoEncontradoException;
 import com.pubbunker.model.Usuario;
-import com.pubbunker.repository.usuarioRepository;
+import com.pubbunker.service.UsuarioService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin("*")
 public class AuthController {
-    private final usuarioRepository usuarioRepository;
 
-    public AuthController(usuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
+    private final UsuarioService usuarioService;
+
+    public AuthController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequestDTO dto) {
-        Optional<Usuario> usuarioOptional =
-                usuarioRepository.findByEmail(dto.getEmail());
-        if(usuarioOptional.isEmpty()) {
+    public ResponseEntity<?> login(
+            @RequestBody LoginRequestDTO dto
+    ) {
+        try {
+            Usuario usuario =
+                    usuarioService.buscarPorEmail(dto.getEmail());
+
+            if (!usuario.getSenha().equals(dto.getSenha())) {
+                return ResponseEntity
+                        .status(HttpStatus.UNAUTHORIZED)
+                        .body("Senha inválida");
+            }
+
+            LoginResponseDTO response =
+                    new LoginResponseDTO(
+                            usuario.getNome(),
+                            usuario.getEmail(),
+                            usuario.getRole()
+                    );
+
+            return ResponseEntity.ok(response);
+
+        } catch (RecursoNaoEncontradoException exception) {
             return ResponseEntity
                     .status(HttpStatus.UNAUTHORIZED)
                     .body("Usuário não encontrado");
         }
-        Usuario usuario = usuarioOptional.get();
-        if(!usuario.getSenha().equals(dto.getSenha())) {
-            return ResponseEntity
-                    .status(HttpStatus.UNAUTHORIZED)
-                    .body("Senha inválida");
-        }
-        LoginResponseDTO response =
-                new LoginResponseDTO(
-                        usuario.getNome(),
-                        usuario.getEmail(),
-                        usuario.getRole()
-                );
-        return ResponseEntity.ok(response);
     }
 }
